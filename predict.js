6989c694c493a8c181259be08dc78d646f470039
tmstani23 <tmstani23@gmail.com>
2018-06-13 19:48:22 -0500

    Update 3
    Clarified correct Bayes formula and began work constructing formula function.

--- predict.js
+++ predict.js
@@ -1,14 +1,9 @@
 
-// Global Variables
 
-var x = []; //Array used to store the information for each variable in Beye's theorem
-var y = [];
-var z = [];
-var answer = 0; //Answer variable for the Beye's theorem button.
-
-let priorHypothesis = prompt("Please enter your prior variable (percent chance your hypothesis is correct).");
-let probTrueWithEvent = prompt("Please enter estimated probability your hypothesis is true given event."); //Takes user input as a prompt
-let probFalseWithEvent = prompt("Please enter probability false given event:");
+//User inputs
+const priorHypothesis = prompt("Please enter your prior variable (percent chance your hypothesis is correct).");
+const probTrueWithEvent = prompt("Please enter estimated probability your hypothesis is true given event."); //Takes user input as a prompt
+const probFalseWithEvent = prompt("Please enter probability false given event:");
 
 
 console.log(priorHypothesis);
@@ -23,16 +18,22 @@ const displayAllProb = (x, y, z) => $("#resultbox").text(`Your prior probability
 	Your entered probability given true: ${y}
 	Probability false given event: ${z}`);  //Displays all the entered variable inputs in the resultbox as text.
 
+// formula: (x*y) / ((x*y) + (z*1 - z*x))
+// x=displayprior / y=displaytrueprob / z = displayfalseprob
 	
+const answer = (x,y,z) => (x*y) / ((x*y) + (z*1 - z*x));
+console.log(answer(displayPrior, displayTrueProb, displayFalseProb));	
+
+
 // 		answerProb(); //Runs answerProb a function to calculate Beye's formula from the user inputs entered
 
-// function answerProb() {
+// function answerProb(x,y,z) {
 // 	//Calculates the formula using multiple variables and updating the global variable answer as it progresses.
 // 	answer = (x*y);
 // 	var bottomAnswer = x*y;
 // 	var bottomAnswer2 = z*1 - z*x;
 // 	var bottomAnswer3 = bottomAnswer + bottomAnswer2;
-// 	answer = answer / bottomAnswer3;
+// 	
 // 	document.getElementById("button3").innerHTML = "Click for new hypothesis";  //Changes the third button text to "Click for new hypothesis"
 // 	//Code below displays all the previous inputs for each variable and the answer in the result box
 // 	$("#resultbox").text("Your prior probability:  	" + x + "	Your entered probability given true: 	" + y + 
