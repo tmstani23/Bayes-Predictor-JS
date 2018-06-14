df66569605225a6ec5801bf3c48fa136da732dd1
Tim Work Laptop <tmstani23@gmail.com>
2018-06-14 15:41:17 -0500

    Update 4
    Added input html elements.  Added function to calculate bayes probability from inputs.  Created function to display answer in result box.

--- predict.js
+++ predict.js
@@ -1,50 +1,38 @@
 
+// Bayes formula: (x*y) / ((x*y) + (z*1 - z*x))
+// x=displayprior / y=displaytrueprob / z = displayfalseprob
 
-//User inputs
-const priorHypothesis = prompt("Please enter your prior variable (percent chance your hypothesis is correct).");
-const probTrueWithEvent = prompt("Please enter estimated probability your hypothesis is true given event."); //Takes user input as a prompt
-const probFalseWithEvent = prompt("Please enter probability false given event:");
+//User inputs from DOM:
 
+const priorHypothesis = document.getElementById("base-input").value; 
+const probTrueWithEvent = document.getElementById("base-input").value; 
+const probFalseWithEvent = document.getElementById("base-input").value; 
 
-console.log(priorHypothesis);
-console.log(probTrueWithEvent);
-console.log(probFalseWithEvent);
+//const getInputs = (priorHypothesis, probTrueWithEvent, probFalseWithEvent) => 
+// const priorHypothesis = parseFloat(prompt("Please enter your prior variable (percent chance your hypothesis is correct)."));
+// const probTrueWithEvent = parseFloat(prompt("Please enter estimated probability your hypothesis is true given event."));
+// const probTrueWithEvent = parseFloat(prompt("Please enter probability false given event:"))
+//Compute answer probability:
+const answer = (x,y,z) => console.log((x*y) / ((x*y) + (z - (z*x))));
+const calcAnswer = answer(priorHypothesis, probTrueWithEvent, probFalseWithEvent);
+
 
 //Display functions:
-const displayPrior = (priorHypothesis) => $("#resultbox").text("Your prior probability: " + priorHypothesis);
-const displayTrueProb = (probTrueWithEvent) => $("#resultbox").text("Probability your prior is true given event: " + probTrueWithEvent);
-const displayFalseProb = (probFalseWithEvent) => $("#resultbox").text("Probability your prior is False given event: " + priorHypothesis);
-const displayAllProb = (x, y, z) => $("#resultbox").text(`Your prior probability: ${x} 
-	Your entered probability given true: ${y}
-	Probability false given event: ${z}`);  //Displays all the entered variable inputs in the resultbox as text.
-
-// formula: (x*y) / ((x*y) + (z*1 - z*x))
-// x=displayprior / y=displaytrueprob / z = displayfalseprob
+// const displayPrior = (priorHypothesis) => $("#resultbox").text("Base rate of situations where hypothesis is true (prior probability): " + priorHypothesis);
+// const displayTrueProb = (probTrueWithEvent) => $("#resultbox").text("Probability of event if hypothesis is correct: " + probTrueWithEvent);
+// const displayFalseProb = (probFalseWithEvent) => $("#resultbox").text("Probability of event if hypothesis is false: " + priorHypothesis);
+// const displayAllProb = (x, y, z) => $("#resultbox").text(`Base rate probability: ${x} 
+// 	Probability of event if hypothesis is true: ${y}
+// 	Probability of event if hypothesis is false: ${z}`);
 	
-const answer = (x,y,z) => (x*y) / ((x*y) + (z*1 - z*x));
-console.log(answer(displayPrior, displayTrueProb, displayFalseProb));	
+//Display answer function:
+// const displayAnswer = answer => $("#resultbox").text(`Probability your hypothesis is correct: ${answer}`);
 
-
-// 		answerProb(); //Runs answerProb a function to calculate Beye's formula from the user inputs entered
-
-// function answerProb(x,y,z) {
-// 	//Calculates the formula using multiple variables and updating the global variable answer as it progresses.
-// 	answer = (x*y);
-// 	var bottomAnswer = x*y;
-// 	var bottomAnswer2 = z*1 - z*x;
-// 	var bottomAnswer3 = bottomAnswer + bottomAnswer2;
-// 	
-// 	document.getElementById("button3").innerHTML = "Click for new hypothesis";  //Changes the third button text to "Click for new hypothesis"
-// 	//Code below displays all the previous inputs for each variable and the answer in the result box
-// 	$("#resultbox").text("Your prior probability:  	" + x + "	Your entered probability given true: 	" + y + 
-// 	"	Probability false given event: 	" + z + "	Probability your hypothesis is correct: 	" + answer);
-// 	$("#resultbox").css({color: "#500805"});  
-// 	//Resets the three variable arrays to empty
-// 	x = [];
-// 	y = [];
-// 	z = [];
 	
-// }
+
+
+
+//console.log(answer(priorHypothesis, probTrueWithEvent, probFalseWithEvent));	
 
 
 
