2ce4ce6761676ad6398609d1aff8e90fb62913bd
tmstani23 <tmstani23@gmail.com>
2018-06-14 19:39:58 -0500

    Update 5
    Experimenting with event listeners and input changes.

--- predict.js
+++ predict.js
@@ -4,19 +4,22 @@
 
 //User inputs from DOM:
 
-const priorHypothesis = document.getElementById("base-input").value; 
-const probTrueWithEvent = document.getElementById("base-input").value; 
-const probFalseWithEvent = document.getElementById("base-input").value; 
+let priorHypothesis = parseFloat(document.getElementById("base-rate").value);
+let probTrueWithEvent = parseFloat(document.getElementById("true-input").value);
+let probFalseWithEvent = parseFloat(document.getElementById("false-input").value);
 
+//console.log(priorHypothesis);
 //const getInputs = (priorHypothesis, probTrueWithEvent, probFalseWithEvent) => 
 // const priorHypothesis = parseFloat(prompt("Please enter your prior variable (percent chance your hypothesis is correct)."));
 // const probTrueWithEvent = parseFloat(prompt("Please enter estimated probability your hypothesis is true given event."));
 // const probTrueWithEvent = parseFloat(prompt("Please enter probability false given event:"))
 //Compute answer probability:
-const answer = (x,y,z) => console.log((x*y) / ((x*y) + (z - (z*x))));
-const calcAnswer = answer(priorHypothesis, probTrueWithEvent, probFalseWithEvent);
+//let calcAnswer = (x,y,z) => console.log((x*y) / ((x*y) + (z - (z*x))));
+//let answer = () => calcAnswer(priorHypothesis, probTrueWithEvent, probFalseWithEvent);
 
-
+function testUpdate(input) {
+    return console.log(input);
+}
 //Display functions:
 // const displayPrior = (priorHypothesis) => $("#resultbox").text("Base rate of situations where hypothesis is true (prior probability): " + priorHypothesis);
 // const displayTrueProb = (probTrueWithEvent) => $("#resultbox").text("Probability of event if hypothesis is correct: " + probTrueWithEvent);
