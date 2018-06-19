e4c7ce2eea7b39203f77d21bf71438b849994e18
tmstani23 <tmstani23@gmail.com>
2018-06-19 15:44:35 -0500

    Update 8
    Added DisplayForm component that returns an input element with label and includes dynamic calls to the App components state values.  Added handle change and handle submit function that update the App components state with the input field values.  Began work adding Bayes calculation function to the App component.

--- predict-react/pred-react.jsx
+++ predict-react/pred-react.jsx
@@ -1,42 +1,67 @@
+function DisplayForm(props) {
+    return (
+        <label>
+            {props.name}
+            <input type="text" name={props.name} value={props.value} onChange={props.onChange} />
+        </label>
+    )
+}
 
-
-//Primary component used for rendering the app:
 class App extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            value: ''
+            priorHypothesis: '',
+            probTrueWithEvent: '',
+            probFalseWithEvent: ''
         };
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
-    updateInputValue(event){
-        //console.log("input field updated with "+evt.target.value);
-        this.setState = ( { value: event.target.value } )
+  
+    handleChange(event) {
+        //this.setState({priorHypothesis: event.target.value});
+        this.setState( {[event.target.name]: event.target.value} );
     }
-    
-    
+  
+    handleSubmit(event) {
+        //alert('A name was submitted: ' + this.state.value);
+        //console.log(event);
+        () => this.handleClick(id)
+        event.preventDefault();
+    }
+    /* calcBayes(x,y,z) {
+        return testAnswer((x*y) / ((x*y) + (z - (z*x))));
+    }
+    testAnswer(input) {
+        return input < 0 || input > 1 || typeof input != "number" 
+        ? displayMsg("Inputs must be a decimal number between 0 and 1")
+        : displayAnswer(`Percent probability your hypothesis is correct: ${(input * 100).toFixed(2)}%`)
+    } */
     render() {
         return (
             <div>
-                <h1>Main App Component Rendering</h1>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.updateInputValue.bind(this)}/> 
-                </label>
-                
-                   
-                
+                <form onSubmit={this.handleSubmit}>
+                    {/* <label>
+                        Prior Hypothesis:
+                        <input type="text" value={this.state.priorHypothesis} onChange={this.handleChange} />
+                    </label> */}
+                    <DisplayForm name="priorHypothesis" value = {this.state.priorHypothesis} onChange={this.handleChange}/>
+                    <DisplayForm name="probTrueWithEvent" value = {this.state.probTrueWithEvent} onChange={this.handleChange}/>
+                    <DisplayForm name="probFalseWithEvent" value = {this.state.probFalseWithEvent} onChange={this.handleChange}/>
+
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>{this.state.priorHypothesis}</p>
+                <p>{this.state.probTrueWithEvent}</p>
+                <p>{this.state.probFalseWithEvent}</p>
             </div>
-        )
+            
+        );
     }
-
 }
-
-
 ReactDOM.render(
-  <App />,
-  document.getElementById('main')
+    <App />,
+    document.getElementById('root')
 );
-
